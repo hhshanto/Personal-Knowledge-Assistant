43d524ac8a8e13e75bda4fa1ddc0ce0ba97e7709
Mohammad Hasan <hasanibnesaleh@gmail.com>
2025-03-07 09:41:36 +0000

    sqlite3 error fixed

--- sqlite_patch.py
+++ sqlite_patch.py
@@ -1,3 +1,10 @@
-import sys
-import pysqlite3
-sys.modules["sqlite3"] = pysqlite3
+try:
+    # Try to import pysqlite3 and patch sqlite3
+    import pysqlite3
+    import sys
+    sys.modules["sqlite3"] = pysqlite3
+    print("Successfully patched sqlite3 with pysqlite3")
+except ImportError:
+    # If pysqlite3 is not available, use the default sqlite3
+    print("pysqlite3 not available, using default sqlite3")
+    pass
